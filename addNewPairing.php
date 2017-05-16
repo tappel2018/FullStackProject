5bf7c4eb00095d35b4ba17457d523e9b31b74353
Nicholas Contreras <ncontreras2018@francisparker.org>
2017-05-16 12:57:07 -0700

    dank overload

--- addNewPairing.php
+++ addNewPairing.php
@@ -34,19 +34,25 @@ try {
 		$conn->exec($submitNewTeacherSQL);
 	}
 
-	$getTeacherIdSQL = "SELECT * FROM teachers";
+	$getWordIdSQL = "SELECT * FROM words WHERE word='$word'";
+	$wordIdSQLResult = $conn->query($getWordIdSQL)->fetchAll();
+	$wordId = $wordIdSQLResult[0]['id'];
 
-	$getIfPairingExistsSQL = "SELECT * FROM teachertoword WHERE teacher='$teacher' AND word='$word'";
+	$getTeacherIdSQL = "SELECT * FROM teachers WHERE name='$teacher'";
+	$teacherIdSQLResult = $conn->query($getTeacherIdSQL)->fetchAll();
+	$teacherId = $teacherIdSQLResult[0]['id'];
+
+	$getIfPairingExistsSQL = "SELECT * FROM teachertoword WHERE teacher='$teacherId' AND word='$wordId'";
 	$pairingExistsResult = $conn->query($getIfPairingExistsSQL)->fetchAll();
 	print("<p>" . sizeof($pairingExistsResult) . "</p>");
 	if (sizeof($pairingExistsResult) == 0) {
 
-		$submitNewPairingSQL = "INSERT INTO teachertoword VALUES ('$teacher', '$word', 1)";
+		$submitNewPairingSQL = "INSERT INTO teachertoword VALUES ('$teacherId', '$wordId', 1)";
 		$conn->exec($submitNewPairingSQL);
 	} else {
 		foreach ($pairingExistsResult as $row) {
 			$pairCount = $row['count'];
-			$updatePairingCountSQL = "UPDATE teachertoword SET count=$pairCount + 1 WHERE teacher='$teacher' AND word='$word'";
+			$updatePairingCountSQL = "UPDATE teachertoword SET count=$pairCount + 1 WHERE teacher='$teacherId' AND word='$wordId'";
 			$conn->exec($updatePairingCountSQL);
 			break;
 		}
